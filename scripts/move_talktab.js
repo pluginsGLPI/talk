48be75cec7a0de052712cc9e0f673c08570b91fe
Alexandre Delaunay <adelaunay@teclib.com>
2014-05-13 09:19:24 +0200

    fix active tabs

--- scripts/move_talktab.js
+++ scripts/move_talktab.js
@@ -5,8 +5,9 @@ window.setTimeout(function() {
    tabpanel.remove(talktab, false);
    tabpanel.insert(0, talktab);
 
-   // active talk tab when followup tabs was selected
-   if (tabpanel.getActiveTab().id == "TicketFollowup$1") {
+   // active talk tab when followup/task/solution tabs was selected
+   var activeTab = tabpanel.getActiveTab().id;
+   if (activeTab == "TicketFollowup$1" || activeTab == "TicketTask$1" || activeTab == "Ticket$2") {
       tabpanel.setActiveTab('PluginTalkTicket$1');
    }
 
