2ddf43f329564313c8e744695f5de5dce6d830f8
Alexandre Delaunay <adelaunay@teclib.com>
2014-05-23 17:16:19 +0200

    add (un)install methods

--- hook.php
+++ hook.php
@@ -1,9 +1,36 @@
 <?php
 
 function plugin_talk_install() {
-   return true;
+   $version = plugin_version_talk();
+   $migration = new Migration($version['version']);
+
+   // Parse inc directory
+   foreach(glob(dirname(__FILE__).'/inc/*') as $filepath) {
+      // Load *.class.php files and get the class name
+      if(preg_match("/inc.(.+)\.class.php/", $filepath, $matches)) {
+         $classname = 'PluginTalk' . ucfirst($matches[1]);
+         include_once($filepath);
+         // If the install method exists, load it
+         if(method_exists($classname, 'install')) {
+            $classname::install($migration);
+         }
+      }
+   }
+   return true ;
 }   
 
 function plugin_talk_uninstall() {
-   return true;
+   // Parse inc directory
+   foreach(glob(dirname(__FILE__).'/inc/*') as $filepath) {
+      // Load *.class.php files and get the class name
+      if(preg_match("/inc.(.+)\.class.php/", $filepath, $matches)) {
+         $classname = 'PluginTalk' . ucfirst($matches[1]);
+         include_once($filepath);
+         // If the install method exists, load it
+         if(method_exists($classname, 'uninstall')) {
+            $classname::uninstall();
+         }
+      }
+   }
+   return true ;
 }
