52f2f1a007bb4845934860f51ba2f95b23612ed0
Alexandre Delaunay <adelaunay@teclib.com>
2014-06-26 09:49:04 +0200

    reverse timeline filter feature (click to show only this type)

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -447,32 +447,34 @@ class PluginTalkTicket extends CommonGLPI {
       $JS = <<<JAVASCRIPT
       Ext.onReady(function() {
          Ext.select('.filter_timeline li a').on('click', function(ev, current_el) {
-            //show all elements in timeline
-            Ext.select('.h_item').removeClass('h_hidden');
+            //hide all elements in timeline
+            Ext.select('.h_item').addClass('h_hidden');
 
+            //reset all elements
             if (this.className == 'reset') {
                Ext.select('.filter_timeline li a img').each(function(el2) {
-                  el2.dom.src = el2.dom.src.replace('_inactive', '');
+                  el2.dom.src = el2.dom.src.replace('_active', '');
                })
+               Ext.select('.h_item').removeClass('h_hidden');
                return;
 ;            }
 
             //activate clicked element
-            Ext.get(this.id).toggleClass('h_inactive');
-            if (current_el.src.indexOf('inactive') > 0) {
-               current_el.src = current_el.src.replace('_inactive', '');
+            Ext.get(this.id).toggleClass('h_active');
+            if (current_el.src.indexOf('active') > 0) {
+               current_el.src = current_el.src.replace('_active', '');
             } else {
-               current_el.src = current_el.src.replace(/\.(png)$/, '_inactive.$1');
+               current_el.src = current_el.src.replace(/\.(png)$/, '_active.$1');
             }
 
             //find active classname
-            inactive_classnames = [];
-            Ext.select('.filter_timeline .h_inactive').each(function(el) {
-               inactive_classnames.push(".h_content."+el.dom.className.replace(' h_inactive', ''));
+            active_classnames = [];
+            Ext.select('.filter_timeline .h_active').each(function(el) {
+               active_classnames.push(".h_content."+el.dom.className.replace(' h_active', ''));
             })
 
-            Ext.select(inactive_classnames.join(', ')).each(function(el){
-               el.parent().addClass('h_hidden');
+            Ext.select(active_classnames.join(', ')).each(function(el){
+               el.parent().removeClass('h_hidden');
             })
          });
       });
