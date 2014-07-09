b0fa4a730ca70320b7901f6c11c916ea97f490ab
Alexandre Delaunay <adelaunay@teclib.com>
2014-07-09 11:45:46 +0200

    include move tabs in dom ready event

--- scripts/move_talktab.js
+++ scripts/move_talktab.js
@@ -1,25 +1,29 @@
-//need a timeout for execute code after tabpanel initialization
-window.setTimeout(function() {
+Ext.onReady(function() {
 
-   //function for move tab
-   this.moveTab = function() {
-      talktab = tabpanel.getComponent('PluginTalkTicket$1');
-      tabpanel.remove(talktab, false);
-      tabpanel.insert(0, talktab);
+   //need a timeout for execute code after tabpanel initialization
+   window.setTimeout(function() {
 
-      // active talk tab when followup/task/solution tabs was selected
-      var activeTab = tabpanel.getActiveTab().id;
-      if (activeTab == "TicketFollowup$1" || activeTab == "TicketTask$1" || activeTab == "Ticket$2") {
-         tabpanel.setActiveTab('PluginTalkTicket$1');
-      }
-   };
-   
-   // check if tabpanel is already loaded, else wait a little more
-   if (typeof tabpanel !== "undefined") {
-      this.moveTab();
-   } else {
-      window.setTimeout(function() {
+      //function for move tab
+      this.moveTab = function() {
+         talktab = tabpanel.getComponent('PluginTalkTicket$1');
+         tabpanel.remove(talktab, false);
+         tabpanel.insert(0, talktab);
+
+         // active talk tab when followup/task/solution tabs was selected
+         var activeTab = tabpanel.getActiveTab().id;
+         if (activeTab == "TicketFollowup$1" || activeTab == "TicketTask$1" || activeTab == "Ticket$2") {
+            tabpanel.setActiveTab('PluginTalkTicket$1');
+         }
+      };
+      
+      // check if tabpanel is already loaded, else wait a little more
+      if (typeof tabpanel !== "undefined") {
          this.moveTab();
-      }, 500)
-   }
-}, 250)
+      } else {
+         window.setTimeout(function() {
+            this.moveTab();
+         }, 250)
+      }
+   }, 250)
+
+});
