dcd1cd5c975539a47d2b465d0fdc9abebe206ed7
Emmanuel Haguet <ehaguet@teclib.com>
2014-09-09 16:58:37 +0200

    add missing locales; see #7

---
+++ locales/en_GB.php
@@ -0,0 +1,18 @@
+<?php
+$LANG['plugin_talk']['error'][1] = 'Access denied';
+
+$LANG['plugin_talk']["title"][2] = "Processing ticket";
+$LANG['plugin_talk']["title"][3] = "Actions historical";
+$LANG['plugin_talk']["title"][4] = "Timeline filter";
+$LANG['plugin_talk']["title"][5] = "Enable Talk Tab";
+$LANG['plugin_talk']["title"][6] = "Enable horizontal split view";
+
+$LANG['plugin_talk']["title"][7] = "Talks";
+
+$LANG['plugin_talk']["oldglpi"][1] = "Reset display options";
+
+$LANG['plugin_talk']["special"][1] = "adds a followup";
+$LANG['plugin_talk']["special"][2] = "adds the item";
+
+$LANG['plugin_talk']['old'][1] = "Validation request";
+$LANG['plugin_talk']['old'][2] = "Validation request answer";
---
+++ locales/fr_FR.php
@@ -0,0 +1,18 @@
+<?php
+$LANG['plugin_talk']['error'][1] = 'Access denied';
+
+$LANG['plugin_talk']["title"][2] = "Traitement du ticket";
+$LANG['plugin_talk']["title"][3] = "Historique des actions";
+$LANG['plugin_talk']["title"][4] = "Filtrer l'historique";
+$LANG['plugin_talk']["title"][5] = "Activer l'onglet Traitement du ticket";
+$LANG['plugin_talk']["title"][6] = "Activer la vue horizontale sur les tickets";
+
+$LANG['plugin_talk']["title"][7] = "Discussions";
+
+$LANG['plugin_talk']["oldglpi"][1] = "Réinitialiser les options d'affichage";
+
+$LANG['plugin_talk']["special"][1] = "ajoute un suivi"; //"%s adds a followup";
+$LANG['plugin_talk']["special"][2] = "ajoute l'élément"; //"%s adds the item";
+
+$LANG['plugin_talk']['old'][1] = "Demande de validation";
+$LANG['plugin_talk']['old'][2] = "Réponse à une demande de validation";
