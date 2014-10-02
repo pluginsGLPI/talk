158d0beefc3f0918541676c9a65d20442aa55c11
Alexandre Delaunay <adelaunay@teclib.com>
2014-10-02 14:46:25 +0200

    add missing js in previous commit

---
+++ scripts/split_button.js
@@ -0,0 +1,17 @@
+split_button = function() {
+   var splitBtn = Ext.get('x-split-button');
+
+   Ext.select('.x-button-drop').on('click', function(event, target, options) {
+         splitBtn.toggleClass('open');
+   });
+
+   Ext.select('.x-split-button').on('click', function(event, target, options) {
+      event.stopPropagation();
+   });
+
+   Ext.select('html').on('click', function(event, target, options) {
+      if (splitBtn.hasClass('open')) {
+         splitBtn.removeClass('open');
+      }
+   });
+}
