618483d445e65ec4d8db1237ec3435dee740ad86
Alexandre Delaunay <adelaunay@teclib.com>
2014-10-09 15:35:48 +0200

    hide status on selection

--- scripts/split_button.js
+++ scripts/split_button.js
@@ -2,14 +2,14 @@ split_button = function() {
    var splitBtn = Ext.get('x-split-button');
 
    Ext.select('.x-button-drop').on('click', function(event, target, options) {
-         splitBtn.toggleClass('open');
+      splitBtn.toggleClass('open');
    });
 
    Ext.select('.x-split-button').on('click', function(event, target, options) {
       event.stopPropagation();
    });
 
-   Ext.select('html').on('click', function(event, target, options) {
+   Ext.select('.x-button-drop-menu li, html').on('click', function(event, target, options) {
       if (splitBtn.hasClass('open')) {
          splitBtn.removeClass('open');
       }
